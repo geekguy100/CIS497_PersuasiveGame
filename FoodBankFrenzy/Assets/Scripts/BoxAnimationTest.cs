f827b2283383f23b02ec81cbf6db152103e32e2a
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-10-28 16:10:52 -0500

    FX ready for gamemanager, opentoclose box animation done

---
+++ FoodBankFrenzy/Assets/Scripts/BoxAnimationTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxAnimationTest : MonoBehaviour
+{
+    public bool isComplete;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        isComplete = false;
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isComplete)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                isComplete = true;
+                animator.SetBool("isComplete", isComplete);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                isComplete = false;
+                animator.SetBool("isComplete", isComplete);
+            }
+        }
+        
+    }
+}
