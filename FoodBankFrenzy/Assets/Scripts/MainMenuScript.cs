ea54f8f93d8f77f93a9aed9b3462704278f1c316
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2020-10-28 16:15:06 -0500

    FInished Pause Menu

---
+++ FoodBankFrenzy/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿/*
+Name: Chandler Wesoloski
+Script: Main Menu Script
+Description: Sets the Main Menu Functionality
+Date: October 28, 2020
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- FoodBankFrenzy/Assets/Scripts/PauseMenuScript.cs
+++ FoodBankFrenzy/Assets/Scripts/PauseMenuScript.cs
@@ -1,22 +1,60 @@
-﻿using System.Collections;
+﻿/*
+Name: Chandler Wesoloski
+Script: Pause Menu Script
+Description: Sets the Pause menu to the Screen and the Button Functionality
+Date: October 28, 2020
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
-    public Button resumeGame;
-    public Button quitGame;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Variables
+    public GameObject PauseMenuUI;
+    public static bool GameIsPaused = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Checks if the Escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //If the game is paused then it will unpause the game
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            //if it is not paused then it will pause the game
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        //Sets the UI back to not being active, and sets the timescale back to default
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void BackToMainMenu()
+    {
+        //Sets the timescale back to default and goes back to the main menu Scene
+        Time.timeScale = 1f;
+    }
+
+    private void Pause()
+    {
+        //Sets the Pause menu active so it can be shown on the screen, and sets the timescale to 0 and sets GameIsPaused to true
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
