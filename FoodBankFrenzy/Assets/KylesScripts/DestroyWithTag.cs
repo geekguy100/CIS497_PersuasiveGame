cd14de7d5ebb90a37b956c073f1b643c1c895248
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2020-11-11 15:38:14 -0600

    food destroyed when dropped on nothing

--- FoodBankFrenzy/Assets/KylesScripts/DestroyWithTag.cs
+++ FoodBankFrenzy/Assets/KylesScripts/DestroyWithTag.cs
@@ -1,6 +1,6 @@
 /*****************************************************************************
 // File Name :         DestroyWithTag.cs
-// Author :            Kyle Grenier
+// Author :            Kyle Grenier, Frank Calabrese
 // Creation Date :     10/21/2020
 //
 // Brief Description : Destroys any game object that enters its trigger
@@ -10,10 +10,24 @@ using UnityEngine;
 public class DestroyWithTag : MonoBehaviour
 {
     [SerializeField] private string theTag = "";
+    public GameObject pickUpCamera;
+
+    private void Start()
+    {
+        pickUpCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag(theTag))
+        if (col.CompareTag(theTag) && pickUpCamera.GetComponent<Pickup>().objectInHand != col.gameObject)
             Destroy(col.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag(theTag) && pickUpCamera.GetComponent<Pickup>().objectInHand != collision.gameObject)
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
