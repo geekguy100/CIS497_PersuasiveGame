e7096352b87bb0199d557ee6b33257b0b06b44c3
Kyle Grenier <kylegrenier@users.noreply.github.com>
2020-11-13 11:56:53 -0600

    Particle effect plays when misclicked cans are destroyed.

--- FoodBankFrenzy/Assets/KylesScripts/DestroyWithTag.cs
+++ FoodBankFrenzy/Assets/KylesScripts/DestroyWithTag.cs
@@ -20,13 +20,23 @@ public class DestroyWithTag : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag(theTag) && pickUpCamera.GetComponent<Pickup>().objectInHand != col.gameObject)
+        {
+            //If this game object destroys misclicked cans and is NOT the conveyor belt destroyer, play a particle effect at mouse position.
+            //Adding 10 to mousePos b/c it defaults to a Z of -10.
+            if (CompareTag("MisclickDestroyer"))
+                GameManager.Instance.SpawnParticle("incorrect", Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10));
             Destroy(col.gameObject);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.CompareTag(theTag) && pickUpCamera.GetComponent<Pickup>().objectInHand != collision.gameObject)
         {
+            //If this game object destroys misclicked cans and is NOT the conveyor belt destroyer, play a particle effect at mouse position.
+            //Adding 10 to mousePos b/c it defaults to a Z of -10.
+            if (CompareTag("MisclickDestroyer"))
+                GameManager.Instance.SpawnParticle("incorrect", Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10));
             Destroy(collision.gameObject);
         }
     }
